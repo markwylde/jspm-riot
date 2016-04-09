b79bdbe84d6714281260c569ab6d13d6a0823a83
Mark Wylde <markwylde@users.noreply.github.com>
2016-04-09 13:26:55 +0100

    Okay, maybe a semicolon just for the import statement

--- tag.js
+++ tag.js
@@ -1,5 +1,5 @@
 var compiler = require('riot-compiler')
 
 exports.translate = function(load) {
-    return compiler.compile('import riot from \'riot\'\n' + load.source)
+    return compiler.compile('import riot from \'riot\';\n' + load.source)
 }
